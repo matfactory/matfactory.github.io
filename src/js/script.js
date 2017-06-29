7e787f5a7f2c7e78c0126e121ffe7495848059f3
micmankos@gmail.com <mankosmike@gmail.com>
2017-06-29 15:42:12 -0400

    Initial commit

---
+++ src/js/script.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function(){
+     $(document).ready(function(){
+      $('.parallax').parallax();
+    });
+    $(".dropdown-button").dropdown();
+    
+  $(document).ready(function(){
+    $('.collapsible').collapsible();
+  });
+});
+
